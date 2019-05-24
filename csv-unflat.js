ab998e640349bc5c1d9a41b75b6c4d322dcdd4d7
Marcin Borkowski <mbork@mbork.pl>
2019-05-24 11:32:44 +0200

    Add the `csv-unflat.js` script

---
+++ csv-unflat.js
@@ -0,0 +1,35 @@
+// Unflatten a csv file
+
+const csvsync = require('csvsync');
+const fs = require('fs');
+
+let data = [];
+let max_column =0;
+let max_row = 0;
+
+const line_to_cell = function line_to_cell(line) {
+	let match = line.match(/^([A-Z]+)([0-9]+):(.*$)/);
+	if (match && match.length === 4) {
+		let [, col_name, row_number, content] = match;
+		let row = parseInt(row_number, 10) - 1;
+		let col = col_number(col_name) - 1;
+		if (!data[row]) {
+			data[row] = [];
+		}
+		data[row][col] = unescape(content);
+	};
+}
+
+let lines = fs.readFileSync(0, 'utf-8').split('\n').map(line_to_cell);
+console.log(csvsync.stringify(data));
+
+// see https://stackoverflow.com/a/46173864/1181665
+function col_number(name) {
+	return name.split('').reduce((r, a) => r * 26 + parseInt(a, 36) - 9, 0)
+}
+
+// lines.forEach(line => console.log(unescape(line)))
+
+function unescape(string) {
+	return string.replace(/\\n/g, '\n').replace(/\\\\/g, '\\');
+}
